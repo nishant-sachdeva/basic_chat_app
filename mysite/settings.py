22113da3089afc7b1731ae9ad909c7b7c9af4308
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2021-08-09 08:33:06 +0530

    basic chat app ready

--- mysite/settings.py
+++ mysite/settings.py
@@ -73,6 +73,15 @@ ASGI_APPLICATION = 'mysite.asgi.application'
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
